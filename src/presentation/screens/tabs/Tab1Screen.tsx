09f110688211a9ae9c38092ce057ef3ee0657cae
CrisAndes <gonzalomorresi@gmail.com>
2024-03-13 12:16:08 -0300

    Se incorpora componente hamburguesa.

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { DrawerActions, useNavigation } from '@react-navigation/native'
+import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
-
+import { HamburgerMenu } from '../../components/shared/HamburgerMenu'
 export const Tab1Screen = () => {
+
+
   return (
     <View>
+      <HamburgerMenu/>
         <Text> Tab1 Screen</Text>
     </View>
   )
--- src/presentation/screens/tabs/Tab3Screen.tsx
+++ src/presentation/screens/tabs/Tab3Screen.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { HamburgerMenu } from '../../components/shared/HamburgerMenu'
 
 export const Tab3Screen = () => {
   return (
     <View>
+       <HamburgerMenu/>
         <Text> Tab3 Screen</Text>
     </View>
   )
